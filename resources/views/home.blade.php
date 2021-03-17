e317732651d7d9b1e0d6b530229e7c54191c812d
AlejandroAkbal <37181533+AlejandroAkbal@users.noreply.github.com>
2021-03-17 23:32:44 +0100

    feat: add links to homepage

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,32 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">{{ __('Dashboard') }}</div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+                    <div class="card-body">
+                        @if (session('status'))
+                            <div class="alert alert-success" role="alert">
+                                {{ session('status') }}
+                            </div>
+                        @endif
 
-                    {{ __('You are logged in!') }}
+                        <ul class="list-group">
+                            <li class="list-group-item list-group-item-action">
+                                <a href="{{route('recipes.index')}}">
+                                    View recipes
+                                </a>
+                            </li>
+                            <li class="list-group-item list-group-item-action"><a href="">Create recipe</a></li>
+                            <li class="list-group-item list-group-item-action"><a href="">View ingredients</a></li>
+                            <li class="list-group-item list-group-item-action"><a href="">Create ingredients</a></li>
+                        </ul>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
