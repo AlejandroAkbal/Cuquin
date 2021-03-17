b10ac1cc6663baac3fba5f714049acc01e8ebd93
AlejandroAkbal <37181533+AlejandroAkbal@users.noreply.github.com>
2021-03-17 23:32:12 +0100

    feat: create recipe controller

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,5 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::resource('recipes', \App\Http\Controllers\RecipesController::class);
