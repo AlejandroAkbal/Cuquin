8e9b1f5c081681f8beb0f8cd53984fb1d9603556
AlejandroAkbal <37181533+AlejandroAkbal@users.noreply.github.com>
2021-03-17 13:04:30 +0100

    feat: create seeders

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // Default user
+        User::factory()->count(1)->create(['email' => 'test@example.com', 'password' => 'password']);
+
+        User::factory()->count(4)->create();
+    }
+}
