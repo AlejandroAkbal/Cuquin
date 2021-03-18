e1406cc7b6a7b3b00664ee7b814a6386cf1b9fb6
AlejandroAkbal <37181533+AlejandroAkbal@users.noreply.github.com>
2021-03-18 11:04:05 +0100

    feat: add message subview

--- resources/views/recipes/index.blade.php
+++ resources/views/recipes/index.blade.php
@@ -4,6 +4,9 @@
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-8">
+
+                @include('shared.messages')
+
                 <div class="card">
                     <div class="card-header">Recipes</div>
 
