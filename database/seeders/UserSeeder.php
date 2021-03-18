6a57cb1792f9fc6c101faa4e586da3bb82b18d8c
AlejandroAkbal <37181533+AlejandroAkbal@users.noreply.github.com>
2021-03-18 17:17:21 +0100

    chore: disable user factory

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -15,7 +15,7 @@ class UserSeeder extends Seeder
     public function run()
     {
         // Default user
-        User::factory()->count(1)->create(['email' => 'test@example.com', 'password' => 'password']);
+//        User::factory()->count(1)->create(['email' => 'test@example.com', 'password' => 'password']);
 
 //        User::factory()->count(4)->create();
     }
