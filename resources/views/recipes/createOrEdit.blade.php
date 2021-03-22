cb2bf5ba8b967fa85aeed889eb003f9018471bbf
Alejandro Akbal <37181533+AlejandroAkbal@users.noreply.github.com>
2021-03-22 23:11:16 +0100

    refactor: improve edit view

--- resources/views/recipes/createOrEdit.blade.php
+++ resources/views/recipes/createOrEdit.blade.php
@@ -17,33 +17,47 @@
                     </div>
                 @endif
 
-                <form action="{{isset($recipe) ? route('recipes.update', $recipe->id) : route('recipes.store')}}"
-                      method="POST">
-                    @csrf
-                    @if(isset($recipe))
-                        @method('PUT')
-                    @endif
+                <div class="card">
 
-                    <div class="form-group">
-                        <label for="name">Name</label>
-                        <input type="text" class="form-control" id="name" name="name"
-                               placeholder="My savoury meal" value="{{old('name', $recipe->name ?? null)}}" required>
+                    <div class="card-header">{{isset($recipe) ? 'Update' : 'Create'}} recipe</div>
+
+                    <div class="card-body">
+
+                        <form
+                            action="{{isset($recipe) ? route('recipes.update', $recipe->id) : route('recipes.store')}}"
+                            method="POST">
+                            @csrf
+                            @if(isset($recipe))
+                                @method('PUT')
+                            @endif
+
+                            <div class="form-group">
+                                <label for="name">Name</label>
+                                <input type="text" class="form-control" id="name" name="name"
+                                       placeholder="My savoury meal" value="{{old('name', $recipe->name ?? null)}}"
+                                       required>
+                            </div>
+
+                            <div class="form-group">
+                                <label for="description">Description</label>
+                                <textarea class="form-control" id="description" name="description" rows="3"
+                                          required>{{old('description', $recipe->description ?? null)}}</textarea>
+                            </div>
+
+                            <div class="form-group">
+                                <label for="instructions">Instructions</label>
+                                <textarea class="form-control" id="instructions" name="instructions" rows="4"
+                                          required>{{old('instructions', $recipe->instructions ?? null)}}</textarea>
+                            </div>
+
+                            <button type="submit"
+                                    class="btn btn-primary">{{isset($recipe) ? 'Update' : 'Create'}}</button>
+
+                        </form>
                     </div>
 
-                    <div class="form-group">
-                        <label for="description">Description</label>
-                        <textarea class="form-control" id="description" name="description" rows="3"
-                                  required>{{old('description', $recipe->description ?? null)}}</textarea>
-                    </div>
+                </div>
 
-                    <div class="form-group">
-                        <label for="instructions">Instructions</label>
-                        <textarea class="form-control" id="instructions" name="instructions" rows="4"
-                                  required>{{old('instructions', $recipe->instructions ?? null)}}</textarea>
-                    </div>
-
-                    <button type="submit" class="btn btn-primary">{{isset($recipe) ? 'Update' : 'Create'}}</button>
-                </form>
 
             </div>
         </div>
