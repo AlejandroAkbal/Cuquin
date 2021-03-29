f4baae734cea78316357ef6aa454162594df2dae
Alejandro Akbal <37181533+AlejandroAkbal@users.noreply.github.com>
2021-03-29 22:46:41 +0200

    chore: do something

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -26,88 +26,85 @@
 
     <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
 
-        <div class="container">
+        <a class="navbar-brand" href="{{ url('/') }}">
+            {{ config('app.name', 'Laravel') }}
+        </a>
 
-            <a class="navbar-brand" href="{{ url('/') }}">
-                {{ config('app.name', 'Laravel') }}
-            </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent" aria-expanded="false"
+                aria-label="{{ __('Toggle navigation') }}">
+            <span class="navbar-toggler-icon"></span>
+        </button>
 
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
-                    aria-controls="navbarSupportedContent" aria-expanded="false"
-                    aria-label="{{ __('Toggle navigation') }}">
-                <span class="navbar-toggler-icon"></span>
-            </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <!-- Left Side Of Navbar -->
+            <ul class="navbar-nav mr-auto">
 
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <!-- Left Side Of Navbar -->
-                <ul class="navbar-nav mr-auto">
+                <li class="nav-item {{Request::routeIs('home') ? 'active' : ''}}">
+                    <a class="nav-link" href="{{ route('home') }}">Home</a>
+                </li>
 
-                    <li class="nav-item {{Request::routeIs('home') ? 'active' : ''}}">
-                        <a class="nav-link" href="{{ route('home') }}">Home</a>
-                    </li>
+                <li class="nav-item {{Request::routeIs('recipes.*') ? 'active' : ''}}">
+                    <a class="nav-link" href="{{ route('recipes.index') }}">Recipes</a>
+                </li>
 
-                    <li class="nav-item {{Request::routeIs('recipes.*') ? 'active' : ''}}">
-                        <a class="nav-link" href="{{ route('recipes.index') }}">Recipes</a>
-                    </li>
+                <li class="nav-item {{Request::routeIs('ingredients.*') ? 'active' : ''}}">
+                    <a class="nav-link" href="{{ route('ingredients.index') }}">Ingredients</a>
+                </li>
 
-                    <li class="nav-item {{Request::routeIs('ingredients.*') ? 'active' : ''}}">
-                        <a class="nav-link" href="{{ route('ingredients.index') }}">Ingredients</a>
-                    </li>
+            </ul>
 
-                </ul>
+            <div class="d-flex flex-column flex-md-row ml-auto">
+                <!-- Right Side Of Navbar -->
+                <ul class="navbar-nav mr-md-2">
 
-                <div class="d-flex flex-column flex-md-row ml-auto">
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav mr-md-2">
+                    <!-- Authentication Links -->
+                    @guest
+                        @if (Route::has('login'))
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                            </li>
+                        @endif
 
-                        <!-- Authentication Links -->
-                        @guest
-                            @if (Route::has('login'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                                </li>
-                            @endif
+                        @if (Route::has('register'))
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                            </li>
+                        @endif
 
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
+                    @else
+                        <li class="nav-item dropdown">
+                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
+                               data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                {{ Auth::user()->name }}
+                            </a>
 
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
-                                   data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                    {{ Auth::user()->name }}
+                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                <a class="dropdown-item" href="{{ route('logout') }}"
+                                   onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                    {{ __('Logout') }}
                                 </a>
 
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest
-                    </ul>
-
-
-                    <!-- Search form -->
-                    <div class="form-inline mt-2 mt-md-0">
-                        <form action="{{route('recipes.index')}}" class="input-group">
-                            <input id="search" class="form-control" type="search" name="search" placeholder="Search"
-                                   aria-label="Search">
-
-                            <div class="input-group-append">
-                                <button class="btn btn-outline-success" type="submit">Search</button>
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                    @csrf
+                                </form>
                             </div>
-                        </form>
-                    </div>
+                        </li>
+                    @endguest
+                </ul>
+
+
+                <!-- Search form -->
+                <div class="form-inline mt-2 mt-md-0">
+                    <form action="{{route('recipes.index')}}" class="input-group">
+                        <input id="search" class="form-control" type="search" name="search" placeholder="Search"
+                               aria-label="Search">
+
+                        <div class="input-group-append">
+                            <button class="btn btn-outline-success" type="submit">Search</button>
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
