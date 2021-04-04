702ea9c1dcec85b92b4d1dbe3c2d5ed4d43a893b
Alejandro Akbal <37181533+AlejandroAkbal@users.noreply.github.com>
2021-04-04 19:52:28 +0200

    fix: fix regression

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,6 +15,9 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
 </head>
 
 <body>
