7c6824e5e6cb283bd5a071458b51c9e899c9be13
Alejandro Akbal <37181533+AlejandroAkbal@users.noreply.github.com>
2021-04-11 16:42:46 +0200

    style: change wording

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -17,7 +17,9 @@
 
         <hr>
 
-        <h3 class="lead">{{ config('app.name') }} allows you to...</h3>
+        <p>Refine your cooking experience and make better meals! </p>
+
+        <h3 class="lead">With {{ config('app.name') }} you can...</h3>
 
         <ul class="list-unstyled space-y-4">
             <li class="d-flex flex-row mt-4 space-x-4">
@@ -37,7 +39,7 @@
                           d="M0 3.75A.75.75 0 01.75 3h7.497c1.566 0 2.945.8 3.751 2.014A4.496 4.496 0 0115.75 3h7.5a.75.75 0 01.75.75v15.063a.75.75 0 01-.755.75l-7.682-.052a3 3 0 00-2.142.878l-.89.891a.75.75 0 01-1.061 0l-.902-.901a3 3 0 00-2.121-.879H.75a.75.75 0 01-.75-.75v-15zm11.247 3.747a3 3 0 00-3-2.997H1.5V18h6.947a4.5 4.5 0 012.803.98l-.003-11.483zm1.503 11.485V7.5a3 3 0 013-3h6.75v13.558l-6.927-.047a4.5 4.5 0 00-2.823.971z"></path>
                 </svg>
 
-                <span>Browse through other user's recipes</span>
+                <span>Browse through many other recipes</span>
             </li>
 
             <li class="d-flex align-items-center flex-row mt-4 space-x-4">
